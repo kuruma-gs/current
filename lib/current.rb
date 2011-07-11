91fb8b7fd89ed01181161f38192bcc292518758f
kuruma3 <kuruma@galileoscope.com>
2011-07-11 19:34:16 +0900

    current::error

--- lib/current.rb
+++ lib/current.rb
@@ -5,6 +5,9 @@ require File.join(File.dirname(__FILE__),'current/controller.rb')
 require File.join(File.dirname(__FILE__),'current/document.rb')
 require File.join(File.dirname(__FILE__),'current/stamp.rb')
 
+
+class Current::Error < StandardError; end
+
 module Current
   VERSION = '0.0.1'
 end
